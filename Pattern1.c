5402525276d92dc1f5c10ee3ace9d85ce18fa753
8067hetmangukiya@gmail.com <hetmangukiya2207>
2023-09-21 23:19:07 +0530

    21/09/2023

---
+++ Pattern1.c
@@ -0,0 +1,16 @@
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// 5 4 3 2 1
+
+#include<stdio.h>
+
+int main(){
+    for (int i = 1; i <= 5; i++) {
+        for (int j = i; j >= 1; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
