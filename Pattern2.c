65278ab6fc15de62fed960eb261cf4a87d7fd7c9
H R Mangukiya <114165239+hetmangukiya2207@users.noreply.github.com>
2023-10-03 00:12:36 +0530

    03/10/2023

--- Pattern2.c
+++ Pattern2.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 void main(){
-    for(int i=1;i<=5;i++){
-        for(int j=i;j>=1;j--){
+    for(int i=5;i>=1;i--){
+        for(int j=i;j<=5;j++){
             printf("%d",j);
         }
         printf("\n");
